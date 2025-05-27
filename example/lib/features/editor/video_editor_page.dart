7aac26573bc621a6079cc7581b6ba9322ef61829
hm21 <alex.frei@hotmail.ch>
2025-05-27 15:43:54 +0200

    fix(layer): resolve issue that 90deg rotation apply layer wrong

--- example/lib/features/editor/video_editor_page.dart
+++ example/lib/features/editor/video_editor_page.dart
@@ -212,7 +212,7 @@ class _VideoEditorPageState extends State<VideoEditorPage> {
       transform: ExportTransform(
         width: parameters.cropWidth,
         height: parameters.cropHeight,
-        rotateTurns: 4 - parameters.rotateTurns,
+        rotateTurns: parameters.rotateTurns,
         x: parameters.cropX,
         y: parameters.cropY,
         flipX: parameters.flipX,
